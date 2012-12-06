2e6d0535d083b79da16975c3074d07ecd5c33116
Tobias Schmid <2toschmid@googlemail.com>
2012-12-06 11:12:43 +0100

    initial regexer

---
+++ regexer/src/main/java/math_ner/regexer/Regexer.java
@@ -0,0 +1,13 @@
+package math_ner.regexer;
+
+/**
+ * Hello world!
+ *
+ */
+public class Regexer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ regexer/src/test/java/math_ner/regexer/RegexerTest.java
@@ -0,0 +1,38 @@
+package math_ner.regexer;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class RegexerTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public RegexerTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( RegexerTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
